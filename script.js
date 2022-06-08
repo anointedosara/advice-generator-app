4632b93b785be2d6bf319e7e1f77257a2ae5c162
anointedosara <101458383+anointedosara@users.noreply.github.com>
2022-06-09 00:05:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const advice = async() => {
+    try {
+        const data = await axios.get(`https://api.adviceslip.com/advice`)
+        console.log(data.data)
+        let id = document.getElementById("p");
+        id.innerText = `ADVICE #${data.data.slip.id}`
+        let advice = document.getElementById("div3");
+        advice.innerText = `"${data.data.slip.advice}"`
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const button = document.querySelector(".div")
+button.addEventListener("click", advice)
